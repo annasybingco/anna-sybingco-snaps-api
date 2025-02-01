7768821f1fa8b1c5cfbd28d5bfe700b10d95436f
annasybingco <sybingcoanna@gmail.com>
2025-02-01 13:26:11 -0500

    update get file for photos

--- routes/photos.js
+++ routes/photos.js
@@ -1,52 +1,62 @@
-import express from 'express';
-import fs from 'fs';
+import express from "express";
+import fs from "fs";
 const router = express.Router();
 
-const data = fs.readFileSync('./data/photos.json', 'utf8');
+const data = fs.readFileSync("./data/photos.json", "utf8");
 const photos = JSON.parse(data);
+
+router.use(express.json());
 
 router.get("/", function (req, res) {
   res.send(photos);
 });
 
-router.get('/:id', (req, res) => {
-    const photo = photos.find(photo => photo.id === req.params.id);
-    
-    if (!photo) {
-      return res.status(404).json({ error: "Photo not found" });
-    }
-  
-    res.json(photo);
-  });
-  
-  export default router;
+router.get("/:id", (req, res) => {
+  const photo = photos.find((photo) => photo.id === req.params.id);
 
+  if (!photo) {
+    return res.status(404).json({ error: "Photo not found" });
+  }
 
-// router.get("/endpoint2/:id/comments", (req, res) => {
-//     const { photoId, commentId } = req.params;
-//     const photo = photos.find(photo => photo.id === photoId);
-    
-//     if (photo) {
-//         const comment = photo.comments.find(comment => comment.id === commentId);
-        
-//         if (comment) {
-//             res.json({ comment });  // Send the found comment as a JSON response
-//         } else {
-//             res.status(404).json({ error: 'Comment not found' });
+  res.json(photo);
+});
+
+router.get("/:id/comments", (req, res) => {
+  const photo = photos.find((photo) => photo.id === req.params.id);
+
+  if (!photo) {
+    return res.status(404).json({ error: "Comment not found" });
+  }
+
+  res.json(photo.comments);
+});
+
+// router.post("/:id/comments", (req, res) => {
+//   console.log(req.body);
+//   const comment = photos[photos.length].id;
+
+//   const newComment = object.assign({ id: comment }, req.body);
+//   photos.push(newComment)
+//   fs.writeFile('./data/photos.json', JSON.stringify(photos), () => {
+//     res.status(201).json({
+//         status: 'great work',
+//         data: {
+//             photos: newComment
 //         }
-//     } else {
-//         res.status(404).json({ error: 'Photo not found' });
-//     }
+//     });
+//   });
 // });
 
-// router.get("/endpoint2/:id/comments", (req, res) => {
-//     const comment = photos.find(photo => comment.id === req.params.id);
-    
-        
-//         if (comment) {
-//             res.json({ comment });  // Send the found comment as a JSON response
-//         } else {
-//             res.status(404).json({ error: 'Comment not found' });
-//         }
-//     }
-// );
+  //     const photo = photos.find(photo => photo.id === req.params.id);
+
+  //     if (!photo) {
+  //       return res.status(404).json({ error: "Photo not found" });
+  //     }
+
+  //     const newComment = req.body;
+
+  //     photo.comments.push(newComment);
+
+  //     res.status(201).json(photo.comments);
+
+export default router;
