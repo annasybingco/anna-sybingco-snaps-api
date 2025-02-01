8afcb75ca9952bce7e7822d04b98fa05a3911bb1
annasybingco <sybingcoanna@gmail.com>
2025-02-01 18:41:21 -0500

    remove commented out code

--- routes/photos.js
+++ routes/photos.js
@@ -1,13 +1,13 @@
 import express from "express";
 import fs from "fs";
-import cors from 'cors';
+import cors from "cors";
 const router = express.Router();
 
 const data = fs.readFileSync("./data/photos.json", "utf8");
 const photos = JSON.parse(data);
 
 router.use(express.json());
-router.use(cors({ origin: 'http://localhost:5173' }));
+router.use(cors({ origin: "http://localhost:5173" }));
 
 router.get("/", function (req, res) {
   res.send(photos);
@@ -39,19 +39,16 @@ router.post("/:id/comments", (req, res) => {
   if (!photo) {
     return res.status(404).json({ error: "Photo not found" });
   }
-
-  // Create a new comment object
   const newComment = {
     name: req.body.name,
     comment: req.body.comment,
-    id: Date.now().toString(), 
-    timestamp: Date.now() 
+    id: Date.now().toString(),
+    timestamp: Date.now(),
   };
 
   photo.comments.push(newComment);
 
   res.status(201).json(photo.comments);
 });
-
 
 export default router;
