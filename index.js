957938db3447859ccd745a0d305138aa37c29feb
annasybingco <sybingcoanna@gmail.com>
2025-02-01 01:22:12 -0500

    udpate photo api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import tagsRoute from './routes/tags.js';
+import photosRoute from './routes/photos.js';
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:5173' }));
@@ -11,6 +12,8 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/tags', tagsRoute);
+
+app.use('/api/photos', photosRoute);
 
 app.listen (8888, function() {
     console.log('listening')
--- routes/photos.js
+++ routes/photos.js
@@ -1,12 +1,42 @@
 import express from 'express';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import fs from 'fs';
+import path from 'path';
 
 const router = express.Router();
-const photos = require('../data/photos.json'); 
 
-router.get('/photos', (req, res) => {
-    res.json(photos)
-})
+const filePath = path.resolve('data', 'photos.json');
+const photosData = fs.readFileSync(filePath, 'utf-8');
+const photos = JSON.parse(photosData);
 
-export default router
+router.get("/endpoint2", function (req, res) {
+  res.send(photos);
+});
+
+router.get("/endpoint2/:id", (req, res) => {
+    const photo = photos.find(photo => photo.id === req.params.id);
+    
+    if (photo) {
+        res.json({ photo });
+    } else {
+        res.status(404).json({ error: 'Photo not found' });
+    }
+});
+
+router.get("/endpoint2/:photoId/comments/", (req, res) => {
+    const { photoId, commentId } = req.params;
+    const photo = photos.find(photo => photo.id === photoId);
+    
+    if (photo) {
+        const comment = photo.comments.find(comment => comment.id === commentId);
+        
+        if (comment) {
+            res.json({ comment });  // Send the found comment as a JSON response
+        } else {
+            res.status(404).json({ error: 'Comment not found' });
+        }
+    } else {
+        res.status(404).json({ error: 'Photo not found' });
+    }
+});
+
+export default router;
