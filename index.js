61c0333cb112d45d31fb9c29211ee15ee2796c26
annasybingco <sybingcoanna@gmail.com>
2025-02-02 11:57:38 -0500

    made images static

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import tagsRoute from './routes/tags.js';
-import photosRoute from './routes/photos.js';
-import 'dotenv/config';
-const port = process.env.PORT || 8000
+import express from "express";
+import cors from "cors";
+import tagsRoute from "./routes/tags.js";
+import photosRoute from "./routes/photos.js";
+import "dotenv/config";
+const port = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.send('Express is running!');
+app.get("/", (req, res) => {
+  res.send("Express is running!");
 });
 
+app.use("/api", tagsRoute);
 
-app.use('/api', tagsRoute);
+app.use("/api/photos", photosRoute);
 
-app.use('/api/photos', photosRoute);
-
-app.listen (port, function() {
-    console.log(`listening on port' ${port}`)
-})
+app.listen(port, function () {
+  console.log(`listening on port' ${port}`);
+});
